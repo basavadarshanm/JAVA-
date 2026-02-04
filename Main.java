33926ce4cd2f46b93dbbc32b0d1431f5af39b2e0
basavadarshanm <basavadarshan06@gmail.com>
2026-02-04 23:21:03 +0530

    Add folders: BasavaDarshan, Bharath, BharathHonakatti, Bhargav S

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hii");  
+        System.out.print("Enter :");
+
+        //Scanner sc = new Scanner(System.in);
+      //  String x = sc.nextLine();
+       //    System.out.print(x);
+
+        Scanner f = new Scanner(System.in);
+        int a = f.nextInt();
+        int b = f.nextInt();
+        System.out.print(a*b);
+        
+    }
+
+}
---
+++ helloworld.java
@@ -0,0 +1,9 @@
+
+
+class helloworld {
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println(a);
+        int b = 5;
+        System.out.println( a + b);
+    }}
